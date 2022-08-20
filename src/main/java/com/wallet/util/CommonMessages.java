b066e3618e9ecc1ec5fa10f1c6a6ac35cc863403
Fabia Yate <fabianyate31@gmail.com>
2022-08-19 22:11:11 -0500

    add class for common messages

---
+++ src/main/java/com/wallet/util/CommonMessages.java
@@ -0,0 +1,12 @@
+package com.wallet.util;
+
+public final class CommonMessages {
+
+    public static final String ERROR_MESSAGE = "Something has failed. Please contact support";
+    public static final String SUCCESS_MESSAGE = "Successful transaction";
+
+    private CommonMessages(){
+
+    }
+
+}
