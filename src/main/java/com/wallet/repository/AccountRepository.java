5b397d1b8c907ca0209408005f0a9b895978e41b
Fabian Yate <fabianyate31@gmail.com>
2022-08-28 22:50:46 -0500

    fix query

--- src/main/java/com/wallet/repository/AccountRepository.java
+++ src/main/java/com/wallet/repository/AccountRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface AccountRepository extends JpaRepository<Account, Integer> {
-    @Query(value = "select a.account_balance from accounts a where a.account_id = :id", nativeQuery = true)
+    @Query(value = "select a.balance from accounts a where a.id = :id", nativeQuery = true)
     Double findAccountBalance(@Param("id") Integer id);
 
     Account findByAccountIdAndUser_UserId(Integer accountId, Integer userId);
